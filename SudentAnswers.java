e297f1cddd8df53015dfb0ab71c8098975f54506
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-10-16 15:11:56 +0300

    Update SudentAnswers.java

--- SudentAnswers.java
+++ SudentAnswers.java
@@ -12,10 +12,11 @@ public class SudentAnswers {
          final int questions = 10;
          System.out.println("Number of students?");
          int students = sc.nextInt();
+        // System.out.println(students);
          char [][] answer = new char [students] [questions];
          for(int i=0; i <students; i++){
         	 String rand = (String)JOptionPane.showInputDialog(null,
-                     "Student?" + (i+1) ,
+                     "Student " + (i+1) + "?",
                      null,
                      2,
                      null,
@@ -37,11 +38,11 @@ public class SudentAnswers {
         	 for(int j=0; j< questions; j++){
         		 if(answer[i][j]== key[j]) score++;
         	 }
-        	 System.out.println("Student "+ i+1 + "'s score is " + score);
+        	 System.out.println("Student "+ i+1 + "'s score is " + (int) score/questions*100 + "%" );
          }
          
          
-         
+       sc.close();
 	}
 
 }
